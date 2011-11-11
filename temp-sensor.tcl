050658517cba9867254ccbd1d79797f9485b0d91
mables <marcus@alkatezh.com>
2011-11-11 14:13:00 +0100

    Added temp check

--- temp-sensor.tcl
+++ temp-sensor.tcl
@@ -30,4 +30,66 @@ set triggerStat "!tempstat"
 
 #----------------------------END OF CONFIG----------------------------------+
 
+# External Pkgs needed
+package require mysqltcl
+# To get db status, include this:
+global mysqlstatus
 
+# Bindings
+bind pub - $triggerTemp pub:temp
+bind pub - $triggerStat pub:stat
+
+# Functions
+proc pub:temp {nick uhost handle chan txt} {
+        global mysqlHost
+        global mysqlPort
+        global mysqlUser
+        global mysqlPassword
+        global mysqlDb
+        set mysqlHandler [mysqlconnect -host $mysqlHost -port $mysqlPort -user $mysqlUser -password $mysqlPassword -db $mysqlDb]
+
+        set msgString " "
+
+        # Get last temperature
+        foreach temps [mysqlsel $mysqlHandler "SELECT t.temperature,ts.name FROM temperatures t,temperatures_sensors ts WHERE t.sensor_id=ts.id AND timestamp > NOW() - INTERVAL 6 MINUTE GROUP BY ts.name$
+                set temperature [lindex $temps 0]
+                set sensor [lindex $temps 1]
+
+                if {$temperature>=0} {
+                        set temperature "\00304[lindex $temps 0]°C"
+                } else {
+                        set temperature "\00302[lindex $temps 0]°C"
+                }
+
+                # ADD the temp to array
+                append msgString "\003$sensor: $temperature\003 "
+
+        }
+
+        if {[string length $msgString] >= 17} {
+                putserv "PRIVMSG $chan $msgString"
+        }
+
+
+        mysqlclose $mysqlHandler 
+        return 0  
+}
+
+proc pub:stat {nick uhost handle chan txt} {
+        global mysqlHost
+        global mysqlPort
+        global mysqlUser
+        global mysqlPassword
+        global mysqlDb
+        set mysqlHandler [mysqlconnect -host $mysqlHost -port $mysqlPort -user $mysqlUser -password $mysqlPassword -db $mysqlDb]
+
+        global highestTemp
+        global lowestTemp
+        
+        # Get stats
+
+        mysqlclose $mysqlHandler
+        return 0
+}
+
+putlog "SQL-Temperature addon loaded"
